4dd0a60eacad39ea37719c6be9c690f4f49c8e61
Mohamed Elabd <mohamed.elabd92@gmail.com>
2021-09-10 21:37:50 +0200

    Create Xcode project

---
+++ PhotoViewer/PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Mohamed Elabd on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
